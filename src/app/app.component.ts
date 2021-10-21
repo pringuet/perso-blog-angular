b9e2c158825d9e8d0c72ad161efd616e59d18132
ophidia <pascal.dominique.ringuet@gmail.com>
2021-10-21 18:33:13 +0100

    Entraînez-vous en créant une application de type blog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Post } from './types/types';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'perso-blog-angular';
+
+  posts: Post[] = [
+    {
+      title: 'Mon premier post',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi corporis suscipit repudiandae tempora harum cupiditate, eveniet temporibus mollitia id expedita deleniti ad atque sunt, voluptates tempore laboriosam voluptas maiores velit!',
+      loveIts: 0,
+      created_at: new Date('11/01/1965 13:50')
+    },
+    {
+      title: 'Mon deuxième post',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab exercitationem vero quo a quasi harum, perspiciatis quibusdam nemo hic, sint nostrum deleniti eligendi tenetur earum. Quibusdam placeat molestiae alias tempore.',
+      loveIts: 0,
+      created_at: new Date('10/17/2020 08:32')
+    },
+    {
+      title: 'Mon troisième post',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi, quo ad distinctio dignissimos optio asperiores alias officiis. Ratione quasi incidunt natus qui, recusandae quia perspiciatis possimus obcaecati animi fugit. Deserunt?',
+      loveIts: 0,
+      created_at: new Date('03/03/2019 17:37')
+    }
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostListComponent } from './components/post-list/post-list.component';
+import { PostListItemComponent } from './components/post-list-item/post-list-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostListComponent,
+    PostListItemComponent
   ],
   imports: [
     BrowserModule,
